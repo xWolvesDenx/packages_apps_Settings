f05189fbe8ac597c53792e08de9d2f4951b91a6c
Meticulus <theonejohnnyd@gmail.com>
2015-07-11 21:09:22 +0200

    Live Volume Steps [2/2]
    -Volume steps changes are instant, no need to reboot!
    
    LiveVolumeSteps: transitioned volume steps volume normalization.
    
    -This calculates an equivalent volume ratio for the new
    volume steps.
    -example: Volume steps are set at 60. Volume is set at 5. That is
    very low volume. Then volume steps are switched to 5. Now you
    are instantly in a state were your volume steps are 5 and your
    volume is 5. That's full volume! This fixes that issue.
    -So it basically keeps the volume from jumping around while your
    changing volume steps. It may be a little lower but should be
    very close.
    
    **** Bringup for Marshmallow by DirtyUnicorns ****
    
    -- set ListPreference values onCreate and onPreferenceChange
    
    -- Use separate methods for updating preferences and
       commiting change to settings provider and AudioService
    
    -- Support for resetting to default values
    
    Change-Id: I06403c703767baedf6d44170e8a0baed91d8d743

--- src/com/android/settings/validus/SystemSettingSwitchPreference.java
+++ src/com/android/settings/validus/SystemSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.gzosp;
+package com.android.settings.validus;
 
 import android.content.Context;
 import android.provider.Settings;
