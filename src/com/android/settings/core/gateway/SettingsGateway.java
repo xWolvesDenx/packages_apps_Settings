1fd1f1a884b9e84539a391656973ea3ad07b60fc
bigrushdog <randall.rushing@gmail.com>
2017-11-14 01:30:47 -0800

    DUI: Initial checkin for Oreo [3/7]
    http://gerrit.dirtyunicorns.com/#/q/topic:DUI-BringUp+(status:open+OR+status:merged)
    
    Change-Id: I5ea7016c221c0e85752180cd784ec56f715e2c02
    
    DU: fix paths for abc

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -134,6 +134,10 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.gzr.wolvesden.WolvesDen;
+import com.gzr.wolvesden.fragments.FlingSettings;
+import com.gzr.wolvesden.fragments.NavbarSettings;
+import com.gzr.wolvesden.fragments.PulseSettings;
+import com.gzr.wolvesden.fragments.SmartbarSettings;
 
 public class SettingsGateway {
 
@@ -258,6 +262,10 @@ public class SettingsGateway {
             DataUsageList.class.getName(),
             WolvesDen.class.getName(),
             RunningServices.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -297,5 +305,9 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.WolvesDenActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName(),
     };
 }
